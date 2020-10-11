6a9fdeb8893cdf3c07da486900d2ed3e151e1303
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-10-11 09:17:51 +0700

    Update

--- ash/css.js
+++ ash/css.js
@@ -9,7 +9,7 @@
     token['\\/\\*[\\s\\S]*?\\*\\/'] = ['com'];
     // Select everything after `}` and before `{`
     token['(^|\\})(\\s*)([^{}]+?)(\\s*)(\\{)'] = [0, 'pun', 0, query, 0, 'pun'];
-    token['([;{])(\\s*)(' + key + ')(\\s*)(:)(\\s*)([\\s\\S]+?)(\\s*)([;}])'] = [0, 'pun', 0, 'key', 0, 'pun', 0, 'val', 0, 'pun'];
+    token['([;{])(\\s*)(' + key + ')(\\s*)(:)(\\s*)([^;}]+?)(\\s*)([;}])'] = [0, 'pun', 0, 'key', 0, 'pun', 0, 'val', 0, 'pun'];
     token[ASH.STR] = ['str'];
     token[ASH.PUN] = ['pun'];
     ASH.token.css = token;
