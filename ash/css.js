692ffc36f9de334683c75724cb94339bf20f2925
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-10-11 10:15:17 +0700

    Update

--- ash/css.js
+++ ash/css.js
@@ -1,7 +1,5 @@
 (function(token, query) {
     let key = '(?:[a-zA-Z_-][\\w-]*)';
-    let f = '\\b(' + key + ')(\\s*)(\\()';
-    let u = ASH.NUM + '(?:[cme]m|ch|ex|in|p[ctx]|rem|v(?:[hw]|max|min)|%)';
     query['\\[[^\\]]+\\]'] = ['que.att'];
     query['::?' + key] = ['que.pse'];
     query['\\.' + key] = ['que.cla'];
@@ -11,18 +9,16 @@
     token['\\/\\*[\\s\\S]*?\\*\\/'] = ['com'];
     // Select everything after `}` and before `{`
     token['(^|\\}|\\b)(\\s*)([^{}]+?)(\\s*)(\\{)'] = [0, 'pun', 0, query, 0, 'pun'];
-    token['(\\s+|\\b)(' + key + ')(\\s*)(:)(\\s*)([^;}]+?)(\\s+|\\b)'] = [0, 0, 'key', 0, 'pun', 0, {
-        [ASH.STR]: ['str'],
-        [u]: ['num'],
-        '#(?:[a-f\\d]{1,2}){3,4}\\b': ['num.hex'],
-        [ASH.NUM]: ['num'],
-        [ASH.URI]: ['uri'],
-        [f]: [0, 'fun', 0, 'pun'],
-        [ASH.PUN]: ['pun'],
-        // Others must be value
-        '[^\\s]+': ['val']
-    }, 0];
+    token['\\b(' + key + ')(\\s*)(:)'] = [0, 'key', 0, 'pun'];
     token[ASH.STR] = ['str'];
+    // Slice -2 to remove the `\b` part
+    token[ASH.NUM.slice(0, -2) + '(?:[cme]m|ch|ex|in|p[ctx]|rem|v(?:[hw]|max|min)|%)\\b'] = ['num'];
+    token['#(?:[a-f\\d]{1,2}){3,4}\\b'] = ['num.hex'];
+    token[ASH.NUM] = ['num'];
+    token[ASH.URI] = ['uri'],
+    token['\\b(' + key + ')(\\s*)(\\()'] = [0, 'fun', 0, 'pun'];
     token[ASH.PUN] = ['pun'];
+    // Others must be value
+    token['[^\\s]+']: ['val'];
     ASH.token.css = token;
 })({}, {});
