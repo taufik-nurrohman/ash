d4acf7648eb288147986cdec052bf34b87bcaf46
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-10-11 11:49:49 +0700

    Update

---
+++ ash/sgml.js
@@ -0,0 +1,24 @@
+(function(token, atr) {
+    // Just like `html` but this ignores `<script>`, `<style>` and `<template>` tag(s)
+    token['<!--[\\s\\S]*?-->'] = ['com'];
+    token['<![^>]+>'] = ['typ'];
+    // Capture SGML attribute(s)
+    let data = '\\s+[^\\s>=]+(?:=(?:' + ASH.STR + '|[^\\s>]+?))?';
+    // Categorize the captured SGML attribute(s)
+    atr[data] = {};
+    // SGML attribute with value
+    atr[data]['(\\s+)([^\\s=\\/>]+)(=)(' + ASH.STR + '|[^\\s=\\/>]+?)'] = [0, 0, 'key', 'pun', 'val'];
+    // SGML attribute without value
+    atr[data]['(\\s+)([^\\s=\\/>]+)'] = [0, 0, 'key'];
+    // Capture a SGML tag
+    let tag = '<\\/?[^\\s]+?(?:\\s[^>]*)?>';
+    // Categorize the captured SGML tag
+    let task = {
+        // Opening tag with attribute(s)
+        '(<)([^\\s]+)(\\s[^>]*)(>)': ['mar', 'pun', 'nam', atr, 'pun'],
+        // Plain tag or a closing tag
+        '(<)(\\/?)([^\\s]+)(>)': ['mar', 'pun', 'pun', 'nam', 'pun']
+    };
+    token[tag] = task;
+    ASH.token.sgml = token;
+})({}, {});
