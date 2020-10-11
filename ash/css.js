3c138ebb9836990002db26e2cd300a9f95bbbe16
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-10-11 11:25:50 +0700

    Update

--- ash/css.js
+++ ash/css.js
@@ -1,18 +1,19 @@
 (function(token, query) {
-    let key = '(?:[a-zA-Z_-][\\w-]*)';
+    let key = '(?:\\\\' + ASH.PUN + '|[a-zA-Z_-][\\w-]*)';
     query['\\[[^\\]]+\\]'] = ['que.att'];
     query['::?' + key] = ['que.pse'];
     query['\\.' + key] = ['que.cla'];
     query['#' + key] = ['que.id'];
     query['@' + key] = ['que.wor'];
     query[key] = ['que.ele'];
+    query[ASH.PUN] = ['pun'];
     token['\\/\\*[\\s\\S]*?\\*\\/'] = ['com'];
     // Select everything after `}` and before `{`
     token['\\b(\\s*)([^{}]+)(\\s*)(\\{)'] = [0, 0, query, 0, 'pun'];
-    token['\\b(' + key + ')(\\s*)(:)'] = [0, 'key', 0, 'pun'];
+    token['(' + key + ')(\\s*)(:)'] = [0, 'key', 0, 'pun'];
     token[ASH.STR] = ['str'];
     // Slice -2 to remove the `\b` part
-    token[ASH.NUM.slice(0, -2) + '(?:[cme]m|ch|ex|in|p[ctx]|rem|v(?:[hw]|max|min)|%)\\b'] = ['num'];
+    token[ASH.NUM.slice(0, -2) + '(?:(?:[cme]m|ch|deg|ex|in|p[ctx]|rem|v(?:[hw]|max|min))\\b|%))'] = ['num'];
     token['#(?:[a-f\\d]{1,2}){3,4}\\b'] = ['num.hex'];
     token[ASH.NUM] = ['num'];
     token[ASH.URI] = ['uri'],
