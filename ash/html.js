3187002bf50fa72a3305444646d539da33e7f27d
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-10-12 19:49:21 +0700

    Update

--- ash/html.js
+++ ash/html.js
@@ -2,23 +2,21 @@
     token['<!--[\\s\\S]*?-->'] = ['com'];
     token['<![^>]+>'] = ['typ'];
     // Capture HTML attribute(s)
-    let data = '\\s+[^\\s>=]+(?:=(?:' + ASH.STR + '|[^\\s=\\/>]+))?\\s*\\/?\\s*';
+    let data = '\\s+[^\\s>=]+(?:=(?:' + ASH.STR + '|[^\\s=\\/>]+))?';
     // Categorize the captured HTML attribute(s)
     atr[data] = {};
     // HTML attribute with value
     atr[data]['(\\s+)([^\\s=\\/>]+)(=)(' + ASH.STR + '|[^\\s=\\/>]+)'] = [0, 0, 'key', 'pun', 'val'];
     // HTML attribute without value
     atr[data]['(\\s+)([^\\s=\\/>]+)'] = [0, 0, 'key'];
-    // Other(s) must be punctuation
-    atr[ASH.PUN] = ['pun'];
     // Capture a HTML tag
     let tag = '<\\/?[^\\s]+?(?:\\s[^>]*)?>';
     // Categorize the captured HTML tag
     let task = {
         // Opening tag with attribute(s)
-        '(<)([^\\s]+)(\\s[^>]*)(>)': ['mar', 'pun', 'nam', atr, 'pun'],
+        '(<)([^\\s]+)(\\s[^>]*)(\\/?)(>)': ['mar', 'pun', 'nam', atr, 'pun', 'pun'],
         // Plain tag or a closing tag
-        '(<)(\\/?)([^\\s]+)(>)': ['mar', 'pun', 'pun', 'nam', 'pun']
+        '(<)(\\/?)([^\\s]+)(\\/?)(>)': ['mar', 'pun', 'pun', 'nam', 'pun', 'pun']
     };
     token['(<script(?:\\s[^>]*)?>)([\\s\\S]*?)(<\\/script>)'] = [0, token, '~js', token];
     token['(<style(?:\\s[^>]*)?>)([\\s\\S]*?)(<\\/style>)'] = [0, token, '~css', token];
